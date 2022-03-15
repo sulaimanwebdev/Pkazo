0de49c7332db4d9569571bea2b8f925dbf66aa38
Muhammad Sulaiman <trendingofyt@gmail.com>
2022-03-15 19:30:29 +0500

    asd

--- components/complete-post-component.tsx
+++ components/complete-post-component.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Link from 'next/link';
 
 
-const completePostComponent = () => {
+const CompletePostComponent = () => {
 
     const [selectedImage, setselectedImage] = useState(1);
 
@@ -172,4 +172,4 @@ const completePostComponent = () => {
   )
 }
 
-export default completePostComponent
+export default CompletePostComponent
